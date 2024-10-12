b1d6190e92f6802001e9fedb8bf0c245b30c843d
starbasil <167671623+starbasil143@users.noreply.github.com>
2024-10-12 17:51:24 -0500

    experiment with mixing and matching different effects

---
+++ src/routes/luke/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import EditorTaskbar from '$lib/components/EditorTaskbar.svelte'
+    import * as Tone from "tone";
+    
+    let synth: Tone.Synth;
+
+    let doFeedback = true;
+    let doReverb = true;
+    //const synth = new Tone.Synth().toDestination();
+
+    function FortniteFunction(): void
+    {
+        synth = new Tone.Synth().toDestination();
+
+        if(doFeedback) // if delay node
+        {
+            const feedbackDelay = new Tone.FeedbackDelay("8n.", .8).toDestination();
+            synth.connect(feedbackDelay);
+        }
+
+        if(doReverb)
+        {
+            const reverb = new Tone.Reverb(10).toDestination();
+            synth.connect(reverb);
+        }
+
+        
+        synth.triggerAttackRelease("C4", "8n");
+    }
+    
+
+</script>
+
+<div class="h-screen flex flex-col bg-zinc-900 font-sans">
+	<EditorTaskbar />
+	<main class="flex flex-1 p-4 pattern-dots-md text-zinc-700 align-middle justify-center">
+		<button class="text-white border-teal-800 border-2 p-2 bg-slate-800 w-64 h-32" on:click={()=>FortniteFunction()}> Fortnite </button>
+	</main>
+
+
+</div>
+
