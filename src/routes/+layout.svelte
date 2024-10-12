f18f00490eadf47a0798a58c081f6407237cdee7
Samantha Nguyen <contact@samanthanguyen.me>
2024-10-12 15:47:46 -0500

    only show header in root

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,6 @@
 	import 'pattern.css/dist/pattern.min.css'
 	import '@fontsource/geist-mono'
 	import '@fontsource/geist-sans'
-	import Header from '$lib/components/Header.svelte'
 </script>
 
-<Header />
 <slot />
