718580414bfc79f3f04c8ae22acb591dc0f55b19
Samantha Nguyen <contact@samanthanguyen.me>
2024-10-13 02:16:16 -0500

    allow naming nodes

--- src/lib/components/node/AudioNode.svelte
+++ src/lib/components/node/AudioNode.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
-	import { IconInfoCircle } from '@tabler/icons-svelte'
+	import { IconInfoCircle, IconPencil } from '@tabler/icons-svelte'
 
 	export let title: string
 </script>
 
-<div class="shadow-2xl shadow-black rounded-lg bg-zinc-900 flex flex-col w-[300px]">
-	<header class="custom-drag-handle bg-blue-700 text-white p-2 rounded-t-md flex flex-row justify-between items-center">
-		{title}
-		<IconInfoCircle size={20} />
-	</header>
-	<section class="grid grid-cols-2 p-2 gap-1 cursor-default">
-		<slot />
-	</section>
+<div class="w-[300px]">
+	<div class="flex flex-row justify-between p-2">
+		<input type="text" placeholder="Name your node..." class="appearance-none bg-transparent placeholder:text-zinc-800 text-white">
+		<IconPencil size={20} />
+	</div>
+		
+	<div class="shadow-2xl shadow-black rounded-lg bg-zinc-900 flex flex-col w-full">
+		<header class="custom-drag-handle bg-blue-700 text-white p-2 rounded-t-md flex flex-row justify-between items-center">
+			{title}
+			<IconInfoCircle size={20} />
+		</header>
+		<section class="grid grid-cols-2 p-2 gap-1 cursor-default">
+			<slot />
+		</section>
+	</div>	
 </div>
 
