f358bb2f251be161038dd89674c8bb90fecd61e7
Samantha Nguyen <contact@samanthanguyen.me>
2024-10-13 02:09:35 -0500

    restyle nodes

--- src/lib/components/node/AudioNode.svelte
+++ src/lib/components/node/AudioNode.svelte
@@ -4,8 +4,8 @@
 	export let title: string
 </script>
 
-<div class="shadow-2xl shadow-black rounded-lg bg-zinc-900 flex flex-col w-[300px] border-2 border-black">
-	<header class="custom-drag-handle bg-black text-white p-2 rounded-t-md flex flex-row justify-between items-center">
+<div class="shadow-2xl shadow-black rounded-lg bg-zinc-900 flex flex-col w-[300px]">
+	<header class="custom-drag-handle bg-blue-700 text-white p-2 rounded-t-md flex flex-row justify-between items-center">
 		{title}
 		<IconInfoCircle size={20} />
 	</header>
